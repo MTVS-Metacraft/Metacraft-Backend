46d64a6e5da7d53d7ddcbc24c4e588f33de2a034
junsyun22 <junsyun22@gmail.com>
2024-11-01 15:28:56 +0900

    First Setup

---
+++ src/main/java/com/yogo/metacraft/MetacraftApplication.java
@@ -0,0 +1,13 @@
+package com.yogo.metacraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetacraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetacraftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yogo/metacraft/MetacraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogo.metacraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetacraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
