178fc6994d9134ddb2e6f1500946f3207c4c8597
otw7917 <otw7917@gmail.com>
2024-11-11 23:12:40 +0900

    refactor: 불필요한 자료 삭제

--- src/main/java/com/yogo/metacraft/mapdata/repository/MapDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yogo.metacraft.mapdata.repository;
-
-import com.yogo.metacraft.mapdata.domain.MapData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MapDataRepository extends JpaRepository<MapData, Long> {
-}
